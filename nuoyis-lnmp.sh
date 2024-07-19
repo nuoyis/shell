47de365d5208d5036f40bac45fb2a84d8058f7cf
诺依阁 <wkkjonlykang@vip.qq.com>
2024-07-19 07:08:24 +0000

    update 1.0.2
    Signed-off-by: 诺依阁 <wkkjonlykang@vip.qq.com>

---
+++ nuoyis-lnmp.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# 诺依阁-lnmp脚本
+
+nuoyis-lnmp-install(){
+        # 快速安装
+        yes | dnf install nginx* php* mariadb*
+        # 编译安装
+}
+
+# nuoyis-lnmp-webadd(){
+
+# }
+nuoyis-lnmp-install
