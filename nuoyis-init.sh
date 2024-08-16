deb08188f0ea041a42eb68dc0e21b7462420c20e
nuoyis <78991709+nuoyis@users.noreply.github.com>
2024-08-16 20:13:20 +0800

    更新v2.1，增加和优化了部分选项和功能

--- nuoyis-init.sh
+++ nuoyis-init.sh
@@ -13,77 +13,105 @@ auth-init-shell="init.nuoyis.net"
 whois=$(whoami)
 nuo_setnetwork_shell=$(ifconfig -a | grep -o '^\w*' | grep -v 'lo')
 
-if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d/" ]; then
-	PM="yum"
-elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
-	PM="apt-get"
+# 检测包管理器
+if command -v yum > /dev/null 2>&1 && [ -d "/etc/yum.repos.d/" ]; then
+    PM="yum"
+elif command -v apt-get > /dev/null 2>&1 && command -v dpkg > /dev/null 2>&1; then
+    PM="apt"
 fi
 
 # 函数类
 
+# 文件/脚本下载类
+nuoyis_download_manager(){
+	if [ -f /usr/bin/curl ];then
+		curl -sSO $1;
+	else 
+		wget $1;
+	fi;
+}
+
 # 系统项启动
 nuoyis_systemctl_manger(){
 	if [ -n $1 ] && [ -n $2 ];then
-		if [ $1 = "start" ];then
-			systemctl enable --now $2
-		fi
+		case $2 in
+			"start")
+				systemctl enable --now $2
+				;;
+			"stop")
+				systemctl stop $2
+				;;
+			"reload")
+				systemctl reload $2
+				;;
+			"poweroff")
+				systemctl poweroff
+				;;
+			"reboot")
+				systemctl reboot
+				;;
+			*)
+				echo "未找到函数请求，请重试"
+		esac
 	fi
 }
 
-# 源安装/更新()
+# 源安装/更新函数
 nuoyis_install_manger(){
 	case $1 in
-		0)
+		"remove")
+			# 移除指定的软件包
 			yes | $PM remove $2 -y
 			;;
-		1)
+		"install")
+			# 安装多个指定的软件包
 			for nuoyis_install in ${@:2}
 			do
 				yes | $PM install $nuoyis_install -y
 			done
 			;;
-		2)
-			if [ $PM = "yum" ];then
+		"update")
+			# 更新所有软件包
+			if [ $PM = "apt" ]; then
 				yes | $PM update
 			fi
 			yes | $PM upgrade
 			;;
-		3)
+		"clean")
+			# 清理缓存
 			$PM clean all
 			;;
-		4)
+		"makecache")
+			# 生成缓存
 			$PM makecache
 			;;
-		5)
-			if [ $PM = "yum" ];then
+		"installfull")
+			# 在Yum中使用特定选项安装软件包
+			if [ $PM = "yum" ]; then
 				yes | $PM $2 install ${@:3} -y
 			fi
 			;;
-		6)
-			if [ $PM = "yum" ];then
+		"updatefull")
+			# 在Yum中使用特定选项更新软件包
+			if [ $PM = "yum" ]; then
 				yes | $PM $2 update ${@:3} -y
 			fi
 			;;		
-		7) 
-			yum list installed | egrep $2 &>/dev/null
+		"installcheck") 
+			# 检查指定的软件包是否安装
+			$PM list installed | egrep $2 &>/dev/null
+			;;
+		"repoadd")
+			if [ $PM = "yum" ]; then
+				# repo添加
+				$PM config-manager --add-repo $2
+			fi
 			;;
 		*)
+			# 无效参数处理
 			echo "错误指令"
 			exit 1
 	esac
-	# if [ $1 -eq 0 ];then
-	# 	yes | $PM remove $2 -y
-	# elif [ $1 -eq 1 ];then
-	# 	yes | $PM install $2 -y
-	# elif [ $1 -eq 2 ];then
-	# 	if [ $PM = "yum" ];then
-	# 		yes | $PM update
-	# 	fi
-	# 	yes | $PM upgrade
-	# elif [ $1 -eq 3 ];then
-	# 	$PM clean all
-	
-	# fi
 }
 
 # 写入文件
@@ -111,17 +139,13 @@ EOF
 # 原有宝塔则卸载
 	if [ -f "/etc/init.d/bt" ] || [ -d "/www/server/panel" ]; then
 		echo -e "此服务器安装过宝塔，正在删除"
-		if [ -f /usr/bin/curl ];then
-			curl -sSO http://download.bt.cn/install/bt-uninstall.sh;
-		else 
-			wget -O install_panel.sh http://download.bt.cn/install/bt-uninstall.sh;
-		fi;
+		nuoyis_download_manager https://download.bt.cn/install/bt-uninstall.sh;
 		echo yes | source bt-uninstall.sh -y
 		rm -rf ./bt-uninstall.sh
 	fi
 
 # 原有环境判断并卸载
-    nuoyis_install_manger 0 nginx* php* mariadb* mysql*
+    nuoyis_install_manger remove nginx* php* mariadb* mysql*
     systemctl disable nginx php-fpm mysqld
 	for service in nginx httpd mysqld pure-ftpd tomcat redis memcached mongodb pgsql tomcat tomcat7 tomcat8 tomcat9 php-fpm-52 php-fpm-53 php-fpm-54 php-fpm-55 php-fpm-56 php-fpm-70 php-fpm-71 php-fpm-72 php-fpm-73
 		do
@@ -147,11 +171,7 @@ EOF
 				echo -e ${service} "\033[32mclean\033[0m"
 			fi
 		done
-	if [ -f /usr/bin/curl ];then
-		curl -sSO https://download.bt.cn/install/install_panel.sh;
-	else 
-		wget -O install_panel.sh https://download.bt.cn/install/install_panel.sh;
-	fi;
+	nuoyis_download_manager https://download.bt.cn/install/install_panel.sh
 	# 安装宝塔
 	echo yes | source install_panel.sh -y
 
@@ -159,12 +179,11 @@ EOF
 
 
 	# 修复宝塔php8.3 无--enable-mbstring问题
-	curl -sSO https://gitee.com/nuoyis/shell/raw/main/btpanel_bug_update/php.sh
+	nuoyis_download_manager https://gitee.com/nuoyis/shell/raw/main/btpanel_bug_update/php.sh
 	mv -f ./php.sh /www/server/panel/install/php.sh 2>/dev/null
 	
 	# 删除残留脚本
 	rm -rf ./install_panel.sh
-	rm -rf ./php.sh
 }
 
 # lnmp安装类
@@ -172,28 +191,28 @@ nuoyis_lnmp_install(){
 	echo "安装lnmp"
 	echo "正在测试中，请晚些时候再执行"
 	if [ $PM = "yum" ];then
-		nuoyis_install_manger 1 pcre pcre-devel zlib zlib-devel libxml2 libxml2-devel readline readline-devel ncurses ncerses-devel perl-devel perl-ExtUtils-Embed openssl-devel
+		nuoyis_install_manger install pcre pcre-devel zlib zlib-devel libxml2 libxml2-devel readline readline-devel ncurses ncerses-devel perl-devel perl-ExtUtils-Embed openssl-devel
     else
-		nuoyis_install_manger 1 apt-transport-https dirmngr software-properties-common ca-certificates libgd-dev libgd2-xpm-dev
+		nuoyis_install_manger install apt-transport-https dirmngr software-properties-common ca-certificates libgd-dev libgd2-xpm-dev
 	fi
 	if [ $nuoyis_lnmp_install_yn = "y" ];then
             # 快速安装
 			if [ $PM = "yum" ];then
 				yes | dnf module reset php
 				yes | dnf module install php:remi-8.2
-				nuoyis_install_manger 1 nginx* php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-json php-redis mariadb*
+				nuoyis_install_manger install nginx* php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-json php-redis mariadb*
 				nuoyis_systemctl_manger start nginx
 				nuoyis_systemctl_manger start php-fpm
 				nuoyis_systemctl_manger start mysqld
 			else
-				nuoyis_install_manger 1 nginx mariadb-server mariadb-client php8.2 php8.2-mysql php8.2-fpm php8.2-gd php8.2-xmlrpc php8.2-curl php8.2-intl php8.2-mbstring php8.2-soap php8.2-zip php8.2-ldap php8.2-xsl php8.2-opcache php8.2-cli php8.2-xml php8.2-common
+				nuoyis_install_manger install nginx mariadb-server mariadb-client php8.2 php8.2-mysql php8.2-fpm php8.2-gd php8.2-xmlrpc php8.2-curl php8.2-intl php8.2-mbstring php8.2-soap php8.2-zip php8.2-ldap php8.2-xsl php8.2-opcache php8.2-cli php8.2-xml php8.2-common
 			fi
     else
             # 编译安装
 			echo "创建lnmp基础文件夹"
 			mkdir -p /$auth-server/{openssl,nginx/{webside,server,conf},php,mysql}
-            wget -O nginx.tar.gz https://mirrors.huaweicloud.com/nginx/nginx-1.27.0.tar.gz
-            tar -xzvf nginx.tar.gz
+            nuoyis_download_manager https://mirrors.huaweicloud.com/nginx/nginx-1.27.0.tar.gz
+            tar -xzvf nginx-1.27.0.tar.gz
             cd nginx-1.27.0
 			id -u ${auth}_web >/dev/null 2>&1
 			if [ $? -eq 1 ];then
@@ -202,13 +221,13 @@ nuoyis_lnmp_install(){
 			if [ -d `whereis openssl | cut -d : -f 2 | awk '{print $1}'` ];then
 				nuoyis_openssl=`whereis openssl | cut -d : -f 2 | awk '{print $1}'`
 			else
-				wget https://shell.nuoyis.net/download/openssl-3.3.1.tar.gz
+				nuoyis_download_manager https://shell.nuoyis.net/download/openssl-3.3.1.tar.gz
 				tar -xzvf openssl-3.3.1.tar.gz openssl-3.3.1/
 				mv -f ./openssl-3.3.1/* /nuoyis-server/openssl/ &> /dev/null
 				nuoyis_openssl="/nuoyis-server/openssl/"
 				rm -rf ./openssl-3.3.1.tar.gz
 			fi;
-			nuoyis_install_manger 1 gd-devel.x86_64
+			nuoyis_install_manger install gd-devel.x86_64
 			./configure --prefix=/$auth-server/nginx/server --with-openssl=${nuoyis_openssl} --user=${auth}_web --group=${auth}_web --with-http_stub_status_module --with-http_ssl_module --with-http_image_filter_module --with-http_gzip_static_module --with-http_gunzip_module --with-ipv6 --with-http_sub_module --with-http_flv_module --with-http_addition_module --with-http_realip_module --with-http_mp4_module --with-http_auth_request_module
 			make && make install
 			mkdir -p /$auth-server/logs/nginx
@@ -346,12 +365,12 @@ EOF
 nuoyis_docker_install(){
 	echo "安装Docker"
 	if [ $PM = "yum" ];then
-	nuoyis_install_manger 1 yum-utils device-mapper-persistent-data lvm2
-	yum config-manager --add-repo https://chinanet.mirrors.ustc.edu.cn/docker-ce/linux/centos/docker-ce.repo
+	nuoyis_install_manger install yum-utils device-mapper-persistent-data lvm2
+	nuoyis_install_manger repoadd https://chinanet.mirrors.ustc.edu.cn/docker-ce/linux/centos/docker-ce.repo
 	fi
 	sed -i 's+download.docker.com+chinanet.mirrors.ustc.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
-	nuoyis_install_manger 4
-	nuoyis_install_manger 1 docker-ce
+	nuoyis_install_manger makecache
+	nuoyis_install_manger install docker-ce
 	mkdir -p /etc/docker
 	touch /etc/docker/daemon.json
 	cat > /etc/docker/daemon.json << EOF
@@ -371,8 +390,8 @@ EOF
 	nuoyis_systemctl_manger start docker
 }
 echo -e "=================================================================="
-echo -e "     诺依阁服务器初始化脚本V2.0"
-echo -e "     更新时间:2024.08.07"
+echo -e "     诺依阁服务器初始化脚本V2.1"
+echo -e "     更新时间:2024.08.16"
 echo -e "     博客地址:https://blog.nuoyis.net"
 echo -e "     \e[31m\e[1m注意1:执行本脚本即同意作者方不承担执行脚本的后果 \e[0m"
 echo -e "     \e[31m\e[1m注意2:当前脚本pid为$$,如果卡死请执行kill -9 $$ \e[0m"
@@ -528,24 +547,24 @@ EOF
 	echo "skip_broken=True" >> /etc/dnf/dnf.conf
 
 	echo "正在配置附加源"
-	nuoyis_install_manger 7 epel
+	nuoyis_install_manger installcheck epel
 	if [ $? -eq 0 ];then
-		nuoyis_install_manger 0 epel-release epel-next-release
+		nuoyis_install_manger remove epel-release epel-next-release
 	fi
 
-	nuoyis_install_manger 7 remi
+	nuoyis_install_manger installcheck remi
 	if [ $? -eq 0 ];then
-		nuoyis_install_manger 0 remi-release-9.4-2.el9.remi.noarch
+		nuoyis_install_manger remove remi-release-9.4-2.el9.remi.noarch
 	fi
 
-	nuoyis_install_manger 7 elrepo
+	nuoyis_install_manger installcheck elrepo
 	if [ $? -eq 0 ];then
-		nuoyis_install_manger 0 elrepo-release.noarch
+		nuoyis_install_manger remove elrepo-release.noarch
 	fi
 
 	rpm --import https://shell.nuoyis.net/download/RPM-GPG-KEY-elrepo.org
 	rpm --import https://mirrors.bfsu.edu.cn/epel/RPM-GPG-KEY-EPEL-9
-	nuoyis_install_manger 1 https://mirrors.bfsu.edu.cn/epel/epel-release-latest-9.noarch.rpm https://mirrors.bfsu.edu.cn/epel/epel-next-release-latest-9.noarch.rpm https://shell.nuoyis.net/download/elrepo-release-9.1-1.el9.elrepo.noarch.rpm
+	nuoyis_install_manger install https://mirrors.bfsu.edu.cn/epel/epel-release-latest-9.noarch.rpm https://mirrors.bfsu.edu.cn/epel/epel-next-release-latest-9.noarch.rpm https://shell.nuoyis.net/download/elrepo-release-9.1-1.el9.elrepo.noarch.rpm
 	sudo sed -e 's!^metalink=!#metalink=!g' \
     -e 's!^#baseurl=!baseurl=!g' \
     -e 's!https\?://download\.fedoraproject\.org/pub/epel!https://mirrors.cernet.edu.cn/epel!g' \
@@ -553,33 +572,38 @@ EOF
     -i /etc/yum.repos.d/epel{,-testing}.repo
 	sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/elrepo.repo
 	sed -i 's#elrepo.org/linux#mirrors.cernet.edu.cn/elrepo#g' /etc/yum.repos.d/elrepo.repo
-	nuoyis_install_manger 1 https://shell.nuoyis.net/download/remi-release-9.rpm
+	nuoyis_install_manger install https://shell.nuoyis.net/download/remi-release-9.rpm
 	else
 		# sudo sed -i -r 's#http://(archive|security).ubuntu.com#https://mirrors.aliyun.com#g' /etc/apt/sources.list && sudo apt-get update
 		echo "debian架构系列正在进入第三方脚本，请注意版本安全"
-		if [ -f /usr/bin/curl ];then
-			curl -sSO https://3lu.cn/main.sh;
-		else 
-			wget -O main.sh https://3lu.cn/main.sh;
-		fi;
+		nuoyis_download_manager https://3lu.cn/main.sh
 		source main.sh
 		echo "yes"
 	fi
 	echo "正在更新源"
-	nuoyis_install_manger 3
-	nuoyis_install_manger 2
-	nuoyis_install_manger 4
+	nuoyis_install_manger clean
+	nuoyis_install_manger update
+	nuoyis_install_manger makecache
 # fi
 
 echo "系统优化类"
 
 echo "配置基础系统文件"
-nuoyis_install_manger 1 dnf-plugins-core bash* vim git wget net-tools tuned dos2unix gcc gcc-c++ make unzip perl perl-IPC-Cmd perl-Test-Simple
+nuoyis_install_manger install dnf-plugins-core bash* vim git wget net-tools tuned dos2unix gcc gcc-c++ make unzip perl perl-IPC-Cmd perl-Test-Simple
 
 
 # 来自https://www.rockylinux.cn
 
 echo "正在对系统进行调优"
+
+if [ $PM = "yum" ];then
+nuoyis_install_manger installfull --disablerepo=\* --enablerepo=elrepo-kernel kernel-ml.x86_64
+nuoyis_install_manger remove kernel-tools-libs.x86_64 kernel-tools.x86_64
+nuoyis_install_manger installfull --disablerepo=\* --enablerepo=elrepo-kernel kernel-ml-tools.x86_64
+else
+	echo "暂时不支持rhel以外系列更新内核"
+fi
+
 nuoyis_systemctl_manger start tuned.service
 tuned-adm profile `tuned-adm recommend`
 cat > /etc/sysctl.conf << EOF
@@ -609,7 +633,7 @@ if [ $nuoyis_docker == "y" ];then
 	nuoyis_docker_install
 fi
 
-#
+# 校园网账号破解多设备
 #echo "自启动firewalld"
 ## 安装并启用firewalld
 #sudo yum install -y firewalld
@@ -628,19 +652,6 @@ fi
 ## 重新加载firewalld规则
 #sudo firewall-cmd --reload
 
-echo "更新内核至最新版"
-if [ $PM = "yum" ];then
-nuoyis_install_manger 5 --disablerepo=\* --enablerepo=elrepo-kernel kernel-ml.x86_64
-nuoyis_install_manger 0 kernel-tools-libs.x86_64 kernel-tools.x86_64
-nuoyis_install_manger 5 --disablerepo=\* --enablerepo=elrepo-kernel kernel-ml-tools.x86_64
-else
-	echo "暂不支持debian系列"
-fi
 rm -rf ./nuoyis-init.sh
 
 echo "安装完毕，向前出发吧"
-
-
-# rpm -ivh --nodeps --force https://mirrors.aliyun.com/rockylinux/9/BaseOS/x86_64/os/Packages/r/rocky-gpg-keys-9.4-1.7.el9.noarch.rpm
-# rpm -ivh --nodeps --force https://mirrors.aliyun.com/rockylinux/9/BaseOS/x86_64/os/Packages/r/rocky-release-9.4-1.7.el9.noarch.rpm
-# rpm -ivh --nodeps --force https://mirrors.aliyun.com/rockylinux/9/BaseOS/x86_64/os/Packages/r/rocky-repos-9.4-1.7.el9.noarch.rpm
